6f5c352375af88df61d2c2f7531a92d0be8b8127
Renan Lisboa <renlp89.developer@gmail.com>
2020-07-06 14:27:26 -0300

    error messages functionalities added

--- script.js
+++ script.js
@@ -4,21 +4,61 @@ const binaryInput = document.querySelector('.content-box-1 input')
 const decimalOutput = document.querySelector('.content-box-1 textarea')
 const decimalInput = document.querySelector('.hide-content-box-2 input')
 const binaryOutput = document.querySelector('.hide-content-box-2 textarea')
-// console.log(decimalOutput)
 
-// Binary to Decimal
+// Error Messages Functionalities
+binaryInput.addEventListener('input', (e) => {
+  // Maximum 8 Digit Error
+  const binDigits = binaryInput.value.length
+  if (binDigits > 8) {
+    const span = document.createElement('span')
+    const errorMsg = document.createTextNode('8 digits max!')
+
+    span.appendChild(errorMsg)
+    span.classList.add('error-msg')
+    document.querySelector('.content-boxes').appendChild(span)
+
+    binaryInput.value = ''
+    
+    setTimeout(() => {
+      document.querySelector('.content-boxes').removeChild(span)
+    }, 2000)
+  } else {
+    // Not a Binary Number Error
+    const binaryValue = binaryInput.value
+    const binInpSplit = binaryValue.split("", binDigits)
+    const binInpFilter = binInpSplit.filter(binInpSplit => binInpSplit != 0 && binInpSplit != 1)
+    // console.log(binInpFilter)
+    
+    if (binInpFilter.length > 0) {
+      const span = document.createElement('span')
+      const errorMsg = document.createTextNode('Not a binary!')
+
+      span.appendChild(errorMsg)
+      span.classList.add('error-msg')
+      document.querySelector('.content-boxes').appendChild(span)
+
+      binaryInput.value = ''
+      
+      setTimeout(() => {
+        document.querySelector('.content-boxes').removeChild(span)
+      }, 2000)
+    }
+  }
+})
+
+// Binary to Decimal Conversion
 convertBtn.addEventListener('click', (e) => {
-  let binaryValue = binaryInput.value
-  let resultDec = parseInt(binaryValue, 2).toString(10)
+  const binaryValue = binaryInput.value
+  const resultDec = parseInt(binaryValue, 2).toString(10)
   
   decimalOutput.value = resultDec
   binaryInput.value = ''
 })
 
-// Decimal to Binary
+// Decimal to Binary Conversion
 convertBtn.addEventListener('click', (e) => {
-  let decimalValue = decimalInput.value
-  let resultBin = parseInt(decimalValue, 10).toString(2)
+  const decimalValue = decimalInput.value
+  const resultBin = parseInt(decimalValue, 10).toString(2)
   
   binaryOutput.value = resultBin
   decimalInput.value = ''
